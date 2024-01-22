e56cfcd8aead1df0be0d1b09ee02676fb374f49b
kogetsu <kogetsu0728@gmail.com>
2024-01-22 18:06:06 +0900

    共通部分をtemplate_baseに

--- template/template_atcoder.hpp
+++ template/template_atcoder.hpp
@@ -1,22 +1,4 @@
-#ifdef LOCAL
-#define _GLIBCXX_DEBUG
-#endif
-#include <bits/stdc++.h>
-#include <atcoder/all>
-using namespace std;
-using namespace atcoder;
-using ll=long long;
-using ld=long double;
-template<class T> using graph=vector<vector<T>>;
-template<class T> using min_priority_queue=priority_queue<T,vector<T>,greater<T>>;
-constexpr int INF32=INT_MAX/2;
-constexpr ll INF64=1LL<<60;
-constexpr int DX[]={0,0,-1,1,-1,-1,1,1};
-constexpr int DY[]={-1,1,0,0,-1,1,-1,1};
-template<class T> inline bool chmax(T &a,T b){return a<b?a=b,true:false;}
-template<class T> inline bool chmin(T &a,T b){return a>b?a=b,true:false;}
-#define rep(i,s,n) for(ll i=(ll)(s);i<(ll)(n);i++)
-#define rrep(i,s,n) for(ll i=(ll)(s);i>=(ll)(n);i--)
+#include "template/template_base.hpp"
 void _main();
 int main(){
     cin.tie(0)->sync_with_stdio(0);
