f1432cb4063ecafb34098215baee44c3427f8c09
kogetsu <kogetsu0728@gmail.com>
2024-03-14 15:43:02 +0900

    DX,DYを4近傍と8近傍に分割, 時計回りに

--- template/template.hpp
+++ template/template.hpp
@@ -4,14 +4,16 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 #endif
-using ll=long long;
-using ld=long double;
+using ll = long long;
+using ld = long double;
 template<class T> using graph=vector<vector<T>>;
 template<class T> using min_priority_queue=priority_queue<T,vector<T>,greater<T>>;
 constexpr int INF32=INT_MAX/2;
 constexpr ll INF64=1LL<<60;
-constexpr array<int,8> DX={0,0,-1,1,-1,-1,1,1};
-constexpr array<int,8> DY={-1,1,0,0,-1,1,-1,1};
+constexpr array<int,4> DX4={0,1,0,-1};
+constexpr array<int,4> DY4={-1,0,1,0};
+constexpr array<int,8> DX8={0,1,1,1,0,-1,-1,-1};
+constexpr array<int,8> DY8={-1,-1,0,1,1,1,0,-1};
 template<class T> inline bool chmax(T &a,T b){return a<b?a=b,true:false;}
 template<class T> inline bool chmin(T &a,T b){return a>b?a=b,true:false;}
 #define rep(i,s,n) for(ll i=(ll)(s);i<(ll)(n);i++)
