c44b36625a6e6e7e26b9806a184113b7c1b19669
kogetsu <kogetsu0728@gmail.com>
2024-04-19 21:13:36 +0900

    軽微な修正

--- template/template.hpp
+++ template/template.hpp
@@ -1,16 +1,22 @@
+#ifndef LOCAL
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#endif
 #include <bits/stdc++.h>
 using namespace std;
+using uint=unsigned int;
 using ll=long long;
+using ull=unsigned long long;
 using ld=long double;
 template<class T> using graph=vector<vector<T>>;
 template<class T> using min_priority_queue=priority_queue<T,vector<T>,greater<T>>;
 constexpr int INF32=INT_MAX/2;
 constexpr ll INF64=1LL<<60;
-constexpr array<int,4> DX4={0,1,0,-1};
-constexpr array<int,4> DY4={-1,0,1,0};
-constexpr array<int,8> DX8={0,1,1,1,0,-1,-1,-1};
-constexpr array<int,8> DY8={-1,-1,0,1,1,1,0,-1};
-inline int popcnt(ll n){return __builtin_popcountll(n);}
+constexpr ld PI=3.14159265358979323846264338327950288419716939937510;
+constexpr array<int,4> DX4={0,1,0,-1}, DY4={-1,0,1,0};
+constexpr array<int,8> DX8={0,1,1,1,0,-1,-1,-1}, DY8={-1,-1,0,1,1,1,0,-1};
+inline int popcnt(ull n){return __builtin_popcountll(n);}
 template<class T> inline bool chmax(T& a,const T& b){return a<b?a=b,true:false;}
 template<class T> inline bool chmin(T& a,const T& b){return b<a?a=b,true:false;}
 #define rep(i,s,n) for(ll i=(ll)(s);i<(ll)(n);i++)
