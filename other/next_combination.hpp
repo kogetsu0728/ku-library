9fbb717b234064267f3b5a73bb936769495aa642
kogetsu <kogetsu0728@gmail.com>
2024-09-12 11:06:48 +0900

    style: 改行の追加

--- other/next_combination.hpp
+++ other/next_combination.hpp
@@ -3,24 +3,31 @@
 /**
  * @brief Next Combination
  */
-template <typename T>
-bool next_combination(const T& begin, const T& end, int k) {
+template <class T>
+bool next_combination(const T& begin, const T& end, const int k) {
     const T& sub = begin + k;
 
-    if (begin == end || begin == sub || end == sub) return false;
+    if (begin == end || begin == sub || end == sub) {
+        return false;
+    }
 
     T src = sub;
     while (begin != src) {
         src--;
-        if ((*src) < (*(end - 1))) {
-            T des = sub;
-            while ((*des) <= (*src)) des++;
-            iter_swap(src, des);
-            rotate(src + 1, des + 1, end);
-            rotate(sub, sub + (end - des) - 1, end);
+
+        if (*src < *(end - 1)) {
+            T dest = sub;
+            while (*dest <= *src) dest++;
+
+            iter_swap(src, dest);
+
+            rotate(src + 1, dest + 1, end);
+            rotate(sub, sub + (end - dest) - 1, end);
+
             return true;
         }
     }
+
     rotate(begin, sub, end);
 
     return false;
