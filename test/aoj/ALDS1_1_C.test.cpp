6c7f8fdfa6ef4ab294e33d40ba774ca2efd7448e
kogetsu <kogetsu0728@gmail.com>
2024-09-12 12:01:22 +0900

    style: format

--- test/aoj/ALDS1_1_C.test.cpp
+++ test/aoj/ALDS1_1_C.test.cpp
@@ -1,25 +1,26 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C"
+#define PROBLEM \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C"
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #include "../../math/sieve_of_eratosthenes.hpp"
 
-int main(){
-	int N;
-	cin >> N;
+int main() {
+    int N;
+    cin >> N;
 
-	SieveOfEratosthenes pr(100000000);
+    SieveOfEratosthenes pr(100000000);
 
-	int ans = 0;
-	for(;N--;){
-		int a;
-		cin >> a;
+    int ans = 0;
+    for (; N--;) {
+        int a;
+        cin >> a;
 
-		if(pr.is_prime(a)) ans++;
-	}
+        if (pr.is_prime(a)) ans++;
+    }
 
-	cout << ans << endl;
+    cout << ans << endl;
 
-	return 0;
+    return 0;
 }
--- test/yosupo/binomial_coefficient_prime_mod.test.cpp
+++ test/yosupo/binomial_coefficient_prime_mod.test.cpp
@@ -8,20 +8,20 @@ using namespace atcoder;
 
 #include "../../math/binomial.hpp"
 
-int main(){
-	int T, M;
-	cin >> T >> M;
+int main() {
+    int T, M;
+    cin >> T >> M;
 
-	modint::set_mod(M);
+    modint::set_mod(M);
 
-	Binomial<modint> bin(min(M-1, 10000000));
+    Binomial<modint> bin(min(M - 1, 10000000));
 
-	for(;T--;){
-		int n, k;
-		cin >> n >> k;
+    for (; T--;) {
+        int n, k;
+        cin >> n >> k;
 
-		cout << bin.c(n, k).val() << '\n';
-	}
+        cout << bin.c(n, k).val() << '\n';
+    }
 
-	return 0;
+    return 0;
 }
