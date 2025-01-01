221c8594f93d6311b2091a7541d5f280a9a65e1f
kogetsu <kogetsu0728@gmail.com>
2025-01-01 22:38:29 +0900

    change: テンプレを使う

--- data_structure/segment_tree.hpp
+++ data_structure/segment_tree.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../template/template.hpp"
+
 /**
  * @brief Segment Tree (セグメント木)
  */
@@ -8,31 +10,41 @@ template <class S, S (*op)(S, S), S (*e)()> class SegmentTree {
     int n;
     vector<S> v;
 
-    void update(int i) { v[i] = op(v[i << 1], v[(i << 1) | 1]); }
+    void update(int i) {
+        v[i] = op(v[i << 1], v[(i << 1) | 1]);
+        return;
+    }
 
   public:
     SegmentTree() : SegmentTree(0) {}
     SegmentTree(int _n) : SegmentTree(vector<S>(_n, e())) {}
-    SegmentTree(const vector<S>& _v) {
-        n = (int)_v.size();
-        v.assign(2 * n, e());
-        for (int i = 0; i < n; i++) v[n + i] = _v[i];
-        for (int i = n - 1; i >= 1; i--) update(i);
+    SegmentTree(const vector<S>& _v)
+        : n(int(_v.size())), v(_v.size() * 2, e()) {
+        rep(i, 0, n) { v[n + i] = _v[i]; }
+
+        rrep(i, n - 1, 1) { update(i); }
     }
 
-    S get(int i) const {
-        i += n;
-        return v[i];
-    }
+    S get(int i) const { return v[i + n]; }
 
     S prod(int l, int r) const {
-        l += n, r += n;
+        l += n;
+        r += n;
+
         S v_l = e(), v_r = e();
         while (l < r) {
-            if (l & 1) v_l = op(v_l, v[l++]);
-            if (r & 1) v_r = op(v[--r], v_r);
-            l >>= 1, r >>= 1;
+            if (l & 1) {
+                v_l = op(v_l, v[l++]);
+            }
+
+            if (r & 1) {
+                v_r = op(v[--r], v_r);
+            }
+
+            l >>= 1;
+            r >>= 1;
         }
+
         return op(v_l, v_r);
     }
 
@@ -43,5 +55,7 @@ template <class S, S (*op)(S, S), S (*e)()> class SegmentTree {
             i >>= 1;
             update(i);
         }
+
+        return;
     }
 };
---
+++ test/data_structure/segment_tree.range_min.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include "../../data_structure/segment_tree.hpp"
+#include "../../template/template.hpp"
+
+ll op(ll a, ll b) { return min(a, b); }
+ll e() { return INF64; }
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<ll> A(N);
+    rep(i, 0, N) { cin >> A[i]; }
+
+    SegmentTree<ll, op, e> seg(A);
+    while (Q--) {
+        int l, r;
+        cin >> l >> r;
+
+        cout << seg.prod(l, r) << LF;
+    }
+
+    return 0;
+}
