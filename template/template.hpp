c77d82b11ee5de69a921983b7d503cbe1de7d36d
kogetsu <kogetsu0728@gmail.com>
2025-01-01 17:10:55 +0900

    add: 可変長repマクロ #2

--- template/template.hpp
+++ template/template.hpp
@@ -17,8 +17,16 @@ constexpr array<int, 8> DY = {-1, 0, 1, 0, -1, 1, 1, -1};
 constexpr char LF = '\n';
 const ld PI = acos(-1);
 
-#define rep(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); (i)++)
-#define rrep(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); (i)--)
+#define overload4(a, b, c, d, e, ...) e
+
+#define rep1(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); (i)++)
+#define rep2(i, a, b, c) for (ll i = (ll)(a); (i) < (ll)(b); (i) += (ll)(c))
+#define rep(...) overload4(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+
+#define rrep1(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); (i)--)
+#define rrep2(i, a, b, c) for (ll i = (ll)(a); (i) >= (ll)(b); (i) -= (ll)(c))
+#define rrep(...) overload4(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
+
 #define all(a) (a).begin(), (a).end()
 #define rall(a) (a).rbegin(), (a).rend()
 
--- test/template/template.test.cpp
+++ test/template/template.test.cpp
@@ -13,8 +13,28 @@ int main() {
     assert(chmax(s, 100LL));
 
     s = 0;
+    rep(i, 0, 10, 1) { s += i; }
+    assert(s == 45);
+
+    s = 0;
+    rep(i, 0, 10, 1) { s += i; }
+    assert(s == 45);
+
+    s = 0;
+    rep(i, 0, 10, 9) { s += i; }
+    assert(s == 9);
+
+    s = 0;
     rrep(i, 9, 0) { s += i; }
     assert(s == 45);
+
+    s = 0;
+    rrep(i, 9, 0, 1) { s += i; }
+    assert(s == 45);
+
+    s = 0;
+    rrep(i, 9, 0, 9) { s += i; }
+    assert(s == 9);
 
     string ans = "Hello World";
 
