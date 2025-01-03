1db3f4f75139d39f01dcc69eef8c9775b8b69957
kogetsu <kogetsu0728@gmail.com>
2025-01-03 11:59:27 +0900

    style: format

--- test/data_structure/rollback_union_find.test.cpp
+++ test/data_structure/rollback_union_find.test.cpp
@@ -1,24 +1,25 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/persistent_unionfind"
 
-#include <bits/stdc++.h>
-using namespace std;
-
 #include "../../data_structure/rollback_union_find.hpp"
+#include "../../template/template.hpp"
 
 int main() {
     int N, Q;
     cin >> N >> Q;
+
     vector<vector<int>> g(Q + 1);
     vector<int> t(Q), k(Q), u(Q), v(Q);
-    for (int i = 0; i < Q; i++) {
+		rep(i, 0, Q){
         cin >> t[i] >> k[i] >> u[i] >> v[i];
-        g[k[i] + 1].push_back(i + 1);
+        g[k[i] + 1].emplace_back(i + 1);
     }
 
     RollbackUnionFind uf(N);
     vector<bool> ans(Q);
+
     auto dfs = [&](auto& self, int now) -> void {
         now--;
+
         if (now != -1) {
             if (t[now] == 0) {
                 uf.merge(u[now], v[now]);
@@ -26,19 +27,25 @@ int main() {
                 ans[now] = uf.same(u[now], v[now]);
             }
         }
+
         for (int nxt : g[now + 1]) {
             self(self, nxt);
         }
+
         if (now != -1 && t[now] == 0) {
             uf.undo();
         }
+
+				return;
     };
 
     dfs(dfs, 0);
 
-    for (int i = 0; i < Q; i++) {
+		rep(i, 0, Q){
         if (t[i] == 1) {
-            cout << (ans[i] ? 1 : 0) << endl;
+            cout << (ans[i] ? 1 : 0) << LF;
         }
     }
+
+		return 0;
 }
