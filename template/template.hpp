1bed229972d2ed42e335097e1aa86e201f27255e
kogetsu <kogetsu0728@gmail.com>
2025-01-05 13:20:30 +0900

    add: IS_LOCAL

--- template/template.hpp
+++ template/template.hpp
@@ -18,24 +18,26 @@ constexpr array<int, 8> DX8 = {1, 1, 0, -1, -1, -1, 0, 1};
 constexpr char LF = '\n';
 const ld PI = acos(-1);
 
+#ifdef LOCAL
+constexpr bool IS_LOCAL = true;
+#else
+constexpr bool IS_LOCAL = false;
+#endif
+
+#define IF_LOCAL if constexpr (IS_LOCAL)
+
+#define all(a) begin(a), end(a)
+#define rall(a) rbegin(a), rend(a)
+
 #define overload4(a, b, c, d, e, ...) e
 
-#define rep1(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); (i)++)
+#define rep1(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); ++(i))
 #define rep2(i, a, b, c) for (ll i = (ll)(a); (i) < (ll)(b); (i) += (ll)(c))
 #define rep(...) overload4(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
 
-#define rrep1(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); (i)--)
+#define rrep1(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); --(i))
 #define rrep2(i, a, b, c) for (ll i = (ll)(a); (i) >= (ll)(b); (i) -= (ll)(c))
 #define rrep(...) overload4(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
-
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-
-#ifdef LOCAL
-#define IF_LOCAL if constexpr (true)
-#else
-#define IF_LOCAL if constexpr (false)
-#endif
 
 template <class T> bool chmin(T& a, const T& b) {
     return (a > b) ? (a = b, true) : false;
