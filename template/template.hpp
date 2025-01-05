000560fa91654c62d8320bdfdba8ac0f31bba2e1
kogetsu <kogetsu0728@gmail.com>
2025-01-05 11:25:34 +0900

    style: column limit 90

--- template/template.hpp
+++ template/template.hpp
@@ -8,8 +8,7 @@ using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 template <class T, bool R = false>
-using heap =
-    priority_queue<T, vector<T>, conditional_t<R, greater<T>, less<T>>>;
+using heap = priority_queue<T, vector<T>, conditional_t<R, greater<T>, less<T>>>;
 
 template <class T> constexpr T INF = numeric_limits<T>::max() / 2;
 constexpr array<int, 4> DY4 = {0, -1, 0, 1};
