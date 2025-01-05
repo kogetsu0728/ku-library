547cd3389d42c4d98800da5805c1bde38e7afa2a
kogetsu7 <kogetsu0728@gmail.com>
2025-01-05 21:47:15 +0900

    [#6] add: assert

--- data_structure/fenwick_tree.hpp
+++ data_structure/fenwick_tree.hpp
@@ -10,14 +10,9 @@ template <class T> class FenwickTree {
     int n;
     vector<T> v;
 
-  public:
-    FenwickTree() : FenwickTree(0) {}
-    FenwickTree(int _n) : n(_n), v(_n, T(0)) {}
-    FenwickTree(const vector<T>& _v) : FenwickTree(_v.size()) {
-        rep(i, 0, _v.size()) { set(i, _v[i]); }
-    }
-
     T sum(int r) const {
+        assert(0 <= r && r <= n);
+
         T res = T(0);
         while (0 < r) {
             res += v[r - 1];
@@ -27,14 +22,28 @@ template <class T> class FenwickTree {
         return res;
     }
 
+  public:
+    FenwickTree() : FenwickTree(0) {}
+    FenwickTree(int _n) : n(_n), v(_n, T(0)) { assert(0 <= n); }
+    FenwickTree(const vector<T>& _v) : FenwickTree(_v.size()) {
+        rep(i, 0, _v.size()) { set(i, _v[i]); }
+    }
+
     T sum(int l, int r) const {
-        assert(l <= r);
+        assert(0 <= l && l <= r && r <= n);
+
         return sum(r) - sum(l);
     }
 
-    T get(int i) const { return sum(i, i + 1); }
+    T get(int i) const {
+        assert(0 <= i && i < n);
+
+        return sum(i, i + 1);
+    }
 
     void add(int i, const T& x) {
+        assert(0 <= i && i < n);
+
         i++;
         while (i <= n) {
             v[i - 1] += x;
@@ -45,6 +54,8 @@ template <class T> class FenwickTree {
     }
 
     void set(int i, const T& x) {
+        assert(0 <= i && i < n);
+
         add(i, x - get(i));
 
         return;
