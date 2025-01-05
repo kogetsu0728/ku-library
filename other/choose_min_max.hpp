6e842408d0de04ff7d3d7d0bcee0ed0894e9a8e6
kogetsu7 <kogetsu0728@gmail.com>
2025-01-05 22:55:27 +0900

    add: brief

--- other/choose_min_max.hpp
+++ other/choose_min_max.hpp
@@ -4,6 +4,9 @@
  * @title Choose Minimum / Maximum
  */
 
+/**
+ * @brief Choose Minimum
+ */
 template <class T> bool chmin(T& a, const T& b) {
     if (a > b) {
         a = b;
@@ -13,6 +16,9 @@ template <class T> bool chmin(T& a, const T& b) {
     return false;
 }
 
+/**
+ * @brief Choose Maximum
+ */
 template <class T> bool chmax(T& a, const T& b) {
     if (a < b) {
         a = b;
