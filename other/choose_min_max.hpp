dce7fc9c18efc13114481adef38c14305a6fa7bc
kogetsu7 <kogetsu0728@gmail.com>
2025-01-05 22:47:51 +0900

    fix: doxygen comment

--- other/choose_min_max.hpp
+++ other/choose_min_max.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 /**
- * @brief Choose Minimum
+ * @title Choose Minimum / Maximum
  */
+
 template <class T> bool chmin(T& a, const T& b) {
     if (a > b) {
         a = b;
@@ -12,9 +13,6 @@ template <class T> bool chmin(T& a, const T& b) {
     return false;
 }
 
-/**
- * @brief Choose Maximum
- */
 template <class T> bool chmax(T& a, const T& b) {
     if (a < b) {
         a = b;
