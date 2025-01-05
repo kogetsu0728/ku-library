eb879eaf9a99ede4c8e3882b1aad36ffa58dd584
kogetsu7 <kogetsu0728@gmail.com>
2025-01-05 22:45:24 +0900

    [#9] refactor: if

--- other/choose_min_max.hpp
+++ other/choose_min_max.hpp
@@ -4,12 +4,22 @@
  * @brief Choose Minimum
  */
 template <class T> bool chmin(T& a, const T& b) {
-    return (a > b) ? (a = b, true) : false;
+    if (a > b) {
+        a = b;
+        return true;
+    }
+
+    return false;
 }
 
 /**
  * @brief Choose Maximum
  */
 template <class T> bool chmax(T& a, const T& b) {
-    return (a < b) ? (a = b, true) : false;
+    if (a < b) {
+        a = b;
+        return true;
+    }
+
+    return false;
 }
