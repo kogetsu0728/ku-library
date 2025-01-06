366391493f63b245d12a0bdac5cfa84d18cced17
kogetsu7 <kogetsu0728@gmail.com>
2025-01-06 18:37:53 +0900

    [#12] add: use template

--- test/tree/heavy_light_decomposition.path_query.test.cpp
+++ test/tree/heavy_light_decomposition.path_query.test.cpp
@@ -1,61 +1,67 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_path_sum"
 
-#include <bits/stdc++.h>
-using namespace std;
-
-#include "../../data_structure/segment_tree.hpp"
 #include "../../tree/heavy_light_decomposition.hpp"
+#include "../../data_structure/segment_tree.hpp"
+#include "../../template/template.hpp"
 
-long long op(long long a, long long b) { return a + b; }
+ll op(ll a, ll b) { return a + b; }
 
-long long e() { return 0LL; }
+ll e() { return 0LL; }
 
 int main() {
     int N, Q;
     cin >> N >> Q;
+
     vector<int> A(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
+    rep(i, 0, N) { cin >> A[i]; }
 
     HeavyLightDecomposition hld(N);
-    for (int i = 0; i < N - 1; i++) {
+    rep(i, 0, N - 1) {
         int u, v;
         cin >> u >> v;
+
         hld.add_edge(u, v);
     }
 
     hld.build();
 
-    SegmentTree<long long, op, e> seg(N);
-    for (int i = 0; i < N; i++) {
+    SegmentTree<ll, op, e> seg(N);
+
+    rep(i, 0, N) {
         hld.node_query(i, [&](int j) -> void {
             seg.set(j, A[i]);
             return;
         });
     }
 
-    for (; Q--;) {
+    while (Q--) {
         int t;
         cin >> t;
+
         if (t == 0) {
             int i;
             cin >> i;
-            long long x;
+
+            ll x;
             cin >> x;
+
             hld.node_query(i, [&](int j) -> void {
                 seg.set(j, seg.get(j) + x);
+
                 return;
             });
         } else {
             int u, v;
             cin >> u >> v;
-            long long ans{};
+
+            ll ans = 0;
+
             hld.path_query(u, v, [&](int l, int r) -> void {
                 ans += seg.prod(l, r);
                 return;
             });
-            cout << ans << endl;
+
+            cout << ans << LF;
         }
     }
 }
