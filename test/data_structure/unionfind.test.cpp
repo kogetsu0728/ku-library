66f292ea10ef62ed5f68d57d100d4f29aeb16f92
kogetsu7 <kogetsu0728@gmail.com>
2025-01-06 18:34:29 +0900

    [#12] add: use template

--- test/data_structure/unionfind.test.cpp
+++ test/data_structure/unionfind.test.cpp
@@ -1,23 +1,24 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
 #include "../../data_structure/union_find.hpp"
+#include "../../template/template.hpp"
 
 int main() {
     int N, Q;
     cin >> N >> Q;
 
     UnionFind uf(N);
-    for (; Q--;) {
+
+    while (Q--) {
         int t, u, v;
         cin >> t >> u >> v;
+
         if (t == 0) {
             uf.merge(u, v);
         } else {
-            cout << (uf.same(u, v) ? 1 : 0) << endl;
+            cout << (uf.same(u, v) ? 1 : 0) << LF;
         }
     }
+
+    return 0;
 }
