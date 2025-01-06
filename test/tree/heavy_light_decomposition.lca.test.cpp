1dc75c1236903d9cc7e6661a90a52db65fb2e75a
kogetsu7 <kogetsu0728@gmail.com>
2025-01-06 18:32:07 +0900

    [#12] add: use template

--- test/tree/heavy_light_decomposition.lca.test.cpp
+++ test/tree/heavy_light_decomposition.lca.test.cpp
@@ -1,15 +1,13 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/lca"
 
-#include <bits/stdc++.h>
-using namespace std;
-
 #include "../../tree/heavy_light_decomposition.hpp"
+#include "../../template/template.hpp"
 
 int main() {
     int N, Q;
     cin >> N >> Q;
     HeavyLightDecomposition hld(N);
-    for (int i = 1; i < N; i++) {
+    rep(i, 0, N) {
         int p;
         cin >> p;
         hld.add_edge(i, p);
@@ -17,9 +15,11 @@ int main() {
 
     hld.build();
 
-    for (; Q--;) {
+    while (Q--) {
         int u, v;
         cin >> u >> v;
-        cout << hld.lca(u, v) << endl;
+        cout << hld.lca(u, v) << LF;
     }
+
+    return 0;
 }
