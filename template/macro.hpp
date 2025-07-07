a7aa40a770c03e2b7ce211c580458be1a96ed01d
kogetsu <kogetsu0728@gmail.com>
2025-07-07 11:55:37 +0900

    [#16] feat: add URL

--- template/macro.hpp
+++ template/macro.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * @title マクロ
+ * @see https://trap.jp/post/1224/
+ */
+
 #include "include.hpp"
 #include "type_alias.hpp"
 
@@ -8,10 +13,12 @@
 
 #define overload4(a, b, c, d, e, ...) e
 
-#define rep1(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); ++(i))
-#define rep2(i, a, b, c) for (ll i = (ll)(a); (i) < (ll)(b); (i) += (ll)(c))
-#define rep(...) overload4(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for (ll i = 0; (i) < (ll)(a); ++(i))
+#define rep2(i, a, b) for (ll i = (ll)(a); (i) < (ll)(b); ++(i))
+#define rep3(i, a, b, c) for (ll i = (ll)(a); (i) < (ll)(b); (i) += (ll)(c))
+#define rep(...) overload4(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
 
-#define rrep1(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); --(i))
-#define rrep2(i, a, b, c) for (ll i = (ll)(a); (i) >= (ll)(b); (i) -= (ll)(c))
-#define rrep(...) overload4(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
+#define rrep1(i, a) for (ll i = (ll)(a); (i) >= 0; --(i))
+#define rrep2(i, a, b) for (ll i = (ll)(a); (i) >= (ll)(b); --(i))
+#define rrep3(i, a, b, c) for (ll i = (ll)(a); (i) >= (ll)(b); (i) -= (ll)(c))
+#define rrep(...) overload4(__VA_ARGS__, rrep3, rrep2, rrep1)(__VA_ARGS__)
