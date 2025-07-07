75590be02fa1fae83187fd0540228e0ae646806f
kogetsu <kogetsu0728@gmail.com>
2025-07-07 11:48:06 +0900

    [#18] feat: add inline

--- template/constant.hpp
+++ template/constant.hpp
@@ -3,10 +3,12 @@
 #include "include.hpp"
 #include "type_alias.hpp"
 
-template <class T> constexpr T INF = numeric_limits<T>::max() / 2;
-constexpr array<int, 4> DY4 = {0, -1, 0, 1};
-constexpr array<int, 4> DX4 = {1, 0, -1, 0};
-constexpr array<int, 8> DY8 = {0, -1, -1, -1, 0, 1, 1, 1};
-constexpr array<int, 8> DX8 = {1, 1, 0, -1, -1, -1, 0, 1};
-constexpr char LF = '\n';
-constexpr ld PI = acos(-1);
+template <class T> inline constexpr T INF = numeric_limits<T>::max() / 2;
+inline constexpr ld PI = acos(-1);
+
+inline constexpr array<int, 4> DY4 = {0, -1, 0, 1};
+inline constexpr array<int, 4> DX4 = {1, 0, -1, 0};
+inline constexpr array<int, 8> DY8 = {0, -1, -1, -1, 0, 1, 1, 1};
+inline constexpr array<int, 8> DX8 = {1, 1, 0, -1, -1, -1, 0, 1};
+
+inline constexpr char LF = '\n';
--- template/is_local.hpp
+++ template/is_local.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifdef ONLINE_JUDGE
-constexpr bool IS_LOCAL = false;
+inline constexpr bool IS_LOCAL = false;
 #else
-constexpr bool IS_LOCAL = true;
+inline constexpr bool IS_LOCAL = true;
 #endif
 
 #define IF_LOCAL if constexpr (IS_LOCAL)
