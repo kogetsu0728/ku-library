33d654eb53f8be03cfc4f04a9406f7350ab1ce18
kogetsu <kogetsu0728@gmail.com>
2025-07-07 12:07:51 +0900

    feat: rename R to REVERSE in heap alias

--- template/type_alias.hpp
+++ template/type_alias.hpp
@@ -5,6 +5,8 @@
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
+
 using ld = long double;
-template <class T, bool R = false>
-using heap = priority_queue<T, vector<T>, conditional_t<R, greater<T>, less<T>>>;
+
+template <class T, bool REVERSE = false>
+using heap = priority_queue<T, vector<T>, conditional_t<REVERSE, greater<T>, less<T>>>;
